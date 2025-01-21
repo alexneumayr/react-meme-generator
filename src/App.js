814a0426cfe7d17cddb33c1765c562d1a8adafc0
alexneumayr <alex.nmr@outlook.com>
2025-01-21 17:10:41 +0100

    Add form tags

--- src/App.js
+++ src/App.js
@@ -8,26 +8,35 @@ export default function App() {
   const [selectedTemplate, setSelectedTemplate] = useState('');
   const [templateList, setTemplateList] = useState([]);
 
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    setSelectedTemplate(templateList);
+  }
+
   return (
     <>
-      <label htmlFor="top-text-input">Top text</label>
-      <input
-        id="top-text-input"
-        value={topText}
-        onChange={(event) => setTopText(event.currentTarget.value)}
-      />{' '}
-      <br />
-      <label htmlFor="bottom-text-input">Bottom text</label>
-      <input
-        id="bottom-text-input"
-        value={bottomText}
-        onChange={(event) => setBottomText(event.currentTarget.value)}
-      />
-      <br />
-      <TemplateSelector
-        templateList={templateList}
-        setTemplateList={setTemplateList}
-      />
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="top-text-input">Top text</label>
+        <input
+          id="top-text-input"
+          value={topText}
+          onChange={(event) => setTopText(event.currentTarget.value)}
+        />
+        <br />
+        <label htmlFor="bottom-text-input">Bottom text</label>
+        <input
+          id="bottom-text-input"
+          value={bottomText}
+          onChange={(event) => setBottomText(event.currentTarget.value)}
+        />
+        <br />
+        <TemplateSelector
+          templateList={templateList}
+          setTemplateList={setTemplateList}
+          onChange={() => console.log('Changes')}
+        />
+        <input type="submit" hidden />
+      </form>
       <br />
       <img
         src={`https://api.memegen.link/images/${selectedTemplate}/${topText}/${bottomText}.png`}
--- src/TemplateSelector.js
+++ src/TemplateSelector.js
@@ -1,23 +1,21 @@
 import { useEffect, useState } from 'react';
 
-export default function TemplateSelector(props) {
+export default function TemplateSelector({ setTemplateList, templateList }) {
   useEffect(() => {
     fetch('https://api.memegen.link/templates/')
       .then((response) => response.json())
       .then((data) => {
-        props.setTemplateList([
-          ...new Map(data.map((v) => [v.id, v])).values(),
-        ]);
+        setTemplateList([...new Map(data.map((v) => [v.id, v])).values()]);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [setTemplateList]);
 
   return (
     <>
       <label htmlFor="template-input">Meme template</label>
-      <input id="template-input" list="templates" />
+      <input id="template-input" list="templates" onChange={set/>
       <datalist id="templates">
-        {props.templateList.map((singleTemplate) => {
+        {templateList.map((singleTemplate) => {
           return (
             <option
               key={`template-${singleTemplate.id}`}
